f451332376a978748206fb42792406a60da94ae3
Neburen <40725217+Neburen@users.noreply.github.com>
2024-06-20 01:48:51 +0200

    Add files via upload

--- inicio.sh
+++ inicio.sh
@@ -9,55 +9,28 @@ function header_info {
 clear
 cat <<"EOF"
 **********************************************************************
-* Ubuntu 22.04 LTS Jellyfin LXC Container Script                     *
-* Script version: 1.0.0                                              *
+* Crear contenedor      *
 **********************************************************************
 EOF
 }
 header_info
 echo -e "Loading..."
-APP="eeeee"
-var_disk="10"
-var_cpu="2"
-var_ram="2048"
-var_os="ubuntu"
-var_version="22.04"
 variables
 color
 catch_errors
 
-function default_settings() {
-  CT_TYPE="1"
-  PW=""
-  CT_ID=$NEXTID
-  HN=$NSAPP
-  DISK_SIZE="$var_disk"
-  CORE_COUNT="$var_cpu"
-  RAM_SIZE="$var_ram"
-  BRG="vmbr0"
-  NET="dhcp"
-  GATE=""
-  APT_CACHER=""
-  APT_CACHER_IP=""
-  DISABLEIP6="no"
-  MTU=""
-  SD=""
-  NS=""
-  MAC=""
-  VLAN=""
-  SSH="yes"
-  VERB="no"
-  echo_default
-}
-
 function update_script() {
 header_info
-if [[ ! -d /usr/lib/jellyfin ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
 msg_info "Updating LXC"
 apt-get update &>/dev/null
 apt-get -y upgrade &>/dev/null
+apt-get install -y curl
+apt-get install -y sudo
 apt-get -y autoremove &>/dev/null
-msg_ok "Updated LXC"
+
+msg_ok "Installed Dependencies"
+
+msg_ok "Updated ${APP} LXC"
 exit
 }
 
