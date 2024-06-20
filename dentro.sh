12e18be35a20046ab9e4bc3b5cb3073503ec0365
Neburen <40725217+Neburen@users.noreply.github.com>
2024-06-20 02:54:51 +0200

    Update dentro.sh

--- dentro.sh
+++ dentro.sh
@@ -5,5 +5,4 @@ apt-get install -y curl
 apt-get install -y wget
 apt-get install -y sudo
 wget https://raw.githubusercontent.com/Neburen/Prox/main/crear_servicio.sh
-apt-get -y autoremove
-apt-get -y autoclean
+apt autoremove -y
