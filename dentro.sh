516e7e233dd77e930a6a915ed8ce6ea885e37c0b
Neburen <40725217+Neburen@users.noreply.github.com>
2024-06-20 02:51:47 +0200

    Update dentro.sh

--- dentro.sh
+++ dentro.sh
@@ -4,26 +4,15 @@
 # Author: tteck (tteckster)
 # License: MIT
 # https://github.com/tteck/Proxmox/raw/main/LICENSE
-
-source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
-color
-verb_ip6
-catch_errors
-setting_up_container
-network_check
-update_os
-
 msg_info "Updating"
-$STD apt update -y
-$STD apt upgrade -y
-$STD apt-get install -y curl
-$STD apt-get install -y wget
-$STD apt-get install -y sudo
+apt update -y
+apt upgrade -y
+apt-get install -y curl
+apt-get install -y wget
+apt-get install -y sudo
 msg_ok "Updated"
-
 wget https://raw.githubusercontent.com/Neburen/Prox/main/crear_servicio.sh
-
 msg_info "Cleaning up"
-$STD apt-get -y autoremove
-$STD apt-get -y autoclean
+apt-get -y autoremove
+apt-get -y autoclean
 msg_ok "Cleaned"
