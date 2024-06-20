3c31e50950b69c441c74431b734c1515621a7dd3
Neburen <40725217+Neburen@users.noreply.github.com>
2024-06-20 12:11:48 +0200

    Update dentro.sh

--- dentro.sh
+++ dentro.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 echo "Actualizar la lista de paquetes disponibles"
-apt-get update
+$STD apt-get update
 
 echo "Actualizar los paquetes instalados"
-apt-get -y upgrade
+$STD apt-get -y upgrade
 
 echo "Instalar herramientas necesarias"
-apt-get install -y curl
-apt-get install -y wget
-apt-get install -y sudo
+$STD apt-get install -y curl
+$STD apt-get install -y wget
+$STD apt-get install -y sudo
 
 echo "Descargar el script adicional"
 wget https://raw.githubusercontent.com/Neburen/Prox/main/crear_servicio.sh
 
 echo "Eliminar paquetes innecesarios"
-apt-get -y autoremove
+$STD apt-get -y autoremove
